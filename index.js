f83a5fb0c91d03ae006f0ac6438830c7cfcc7790
Tom Hummel <tphummel@gmail.com>
2014-01-20 14:01:41 -0800

    bug: fixed scoping mistake. wrapped in IIFE for good measure

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-var multilevel = require("multilevel"),
-    net        = require("net"),
-    reconnect  = require("reconnect"),
-    url        = require("url"),
-    createClient;
+(function(){
+  var multilevel = require("multilevel"),
+      net        = require("net"),
+      reconnect  = require("reconnect"),
+      url        = require("url"),
+      createClient;
 
-createClient = function(opts) {
-  db = multilevel.client();
+  createClient = function(opts) {
+    var db = multilevel.client(),
+        strm;
 
-  strm = reconnect(function(conn){
-    console.log("[multilevel-client] connected ", new Date);
-    conn.pipe(db.createRpcStream()).pipe(conn);
+    strm = reconnect(function(conn){
+      console.log("[multilevel-client] connected ", new Date);
+      conn.pipe(db.createRpcStream()).pipe(conn);
 
-  }).connect(opts);
-  
-  strm.on("disconnect", function(){
-    console.log("[multilevel-client] disconnect", new Date);
-  });
+    }).connect(opts);
+    
+    strm.on("disconnect", function(){
+      console.log("[multilevel-client] disconnect", new Date);
+    });
 
-  strm.on("backoff", function(attempts, delay){
-    console.log("[multilevel-client] backoff reconnect", attempts, new Date);
-  });
+    strm.on("backoff", function(attempts, delay){
+      console.log("[multilevel-client] backoff reconnect", attempts, new Date);
+    });
 
-  strm.on("reconnect", function(){
-    console.log("[multilevel-client] reconnect attempting", new Date);
-  });
-  
-  db.reconnectOff = function(){
-    strm.reconnect = false;
+    strm.on("reconnect", function(){
+      console.log("[multilevel-client] reconnect attempting", new Date);
+    });
+    
+    db.reconnectOff = function(){
+      strm.reconnect = false;
+    }
+
+    return db
+
   }
 
-  return db
-
-}
-
-module.exports = createClient;
+  module.exports = createClient;
+})();
