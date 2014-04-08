73e42213aa7a25c2b5ba16c8f734ef2ba84562cc
Tom Hummel <tphummel@gmail.com>
2014-04-07 18:06:05 -0700

    pull logger into fn

--- index.js
+++ index.js
@@ -2,37 +2,38 @@
   var multilevel = require("multilevel"),
       reconnect  = require("reconnect-net"),
       url        = require("url"),
-      createClient;
+      logger, createClient;
+
+  logger = function(msg){
+    console.log('[multilevel-client]', new Date().toISOString(), '|', msg);
+  }
 
   createClient = function(opts) {
     var db = multilevel.client(),
         strm;
 
     strm = reconnect(function(conn){
+      conn.on('error', logger);
 
-      conn.on('error', function(e){
-        console.log('[multilevel-client] tcp err:', e);
-      });
+      logger('connected');
 
-      console.log("[multilevel-client] connected ", new Date);
       var rpc = db.createRpcStream();
-      rpc.on('error', function(e){
-        console.log('[multilevel-client] rpcStreamError: ', e, new Date)
-      });
+      rpc.on('error', logger);
+
       conn.pipe(rpc).pipe(conn);
 
     }).connect(opts);
 
     strm.on("disconnect", function(){
-      console.log("[multilevel-client] disconnect", new Date);
+      logger('disconnect');
     });
 
     strm.on("backoff", function(attempts, delay){
-      console.log("[multilevel-client] backoff reconnect", attempts, new Date);
+      logger('backoff '+attempts);
     });
 
     strm.on("reconnect", function(){
-      console.log("[multilevel-client] reconnect attempting", new Date);
+      logger('reconnect attempting');
     });
 
     db.reconnectOff = function(){
